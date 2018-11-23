e1639fcf8887d81bce2ee52c115795e2b2a33dea
nick.marchini <nick.marchini@slalom.com>
2018-11-23 13:14:23 +0000

    reverting to us-central1

--- common/build/bigquery.sh
+++ common/build/bigquery.sh
@@ -5,7 +5,7 @@ source ../project_settings.sh
 
 # Create the dataset
 # https://cloud.google.com/bigquery/docs/dataset-locations
-bq --location=EU mk --dataset $PROJECT_NAME:"app_dataset_$ENV_TYPE"
+bq --location=US mk --dataset $PROJECT_NAME:"app_dataset_$ENV_TYPE"
 
 
 bq mk --external_table_definition=bigquery_table_def.json "app_dataset_$ENV_TYPE.items"
