e64458c5f951691783284bafb1fc01f1c78fac9b
nick.marchini <nick.marchini@slalom.com>
2018-11-23 11:35:05 +0000

    bigtable changes

--- common/build/bigtable.sh
+++ common/build/bigtable.sh
@@ -8,26 +8,26 @@ source ../project_settings.sh
 declare -a TABLE_FAMILIES=("item" "buyer" "seller")
 
 
-# Is this a production instance or development. Prod will be more expensive and ready for production workloads. 
+# Is this a production instance or development. Prod will be more expensive and ready for production workloads.
 # Development is for non-production workloads.
 BIGTABLE_INSTANCE_TYPE="development"
 # This can be set to hdd or ssd. However, only use this for production.
 BIGTABLE_CLUSTER_STORAGE_TYPE="hdd"
-# You don't want to use the cluster-storage-type for dev environments. 
+# You don't want to use the cluster-storage-type for dev environments.
 BIGTABLE_CLUSTER_STORAGE_TYPE_FLAG="--cluster-storage-type="
 # This will combine the flag and the value, but only for production workloads. As determined by the BIGTABLE_INSTANCE_TYPE variable.
 BIGTABLE_CLUSTER_STORAGE_TYPE_FLAG_VALUE=""
 
 # How many nodes should we use? For production, use at least 3. Doesn't matter for development
-CLUSTER_NUM_NODES="3"
-# You don't want to use the cluster-num-nodes for dev environments. 
+CLUSTER_NUM_NODES="1"
+# You don't want to use the cluster-num-nodes for dev environments.
 BIGTABLE_CLUSTER_NUM_NODES_FLAG="--cluster-num-nodes="
 # This will combine the flag and the value, but only for production workloads. As determined by the BIGTABLE_INSTANCE_TYPE variable.
 BIGTABLE_CLUSTER_NUM_NODES_FLAG_VALUE=""
 # If this is a production environment, combine the flag and the value for storage type.
 
 if [[ $BIGTABLE_INSTANCE_TYPE == "production" ]]; then
-    BIGTABLE_CLUSTER_STORAGE_TYPE_FLAG_VALUE="$BIGTABLE_CLUSTER_STORAGE_TYPE_FLAG$BIGTABLE_CLUSTER_STORAGE_TYPE"        
+    BIGTABLE_CLUSTER_STORAGE_TYPE_FLAG_VALUE="$BIGTABLE_CLUSTER_STORAGE_TYPE_FLAG$BIGTABLE_CLUSTER_STORAGE_TYPE"
     BIGTABLE_CLUSTER_NUM_NODES_FLAG_VALUE="$BIGTABLE_CLUSTER_NUM_NODES_FLAG$CLUSTER_NUM_NODES"
 fi
 
@@ -39,7 +39,7 @@ gcloud beta bigtable instances create $BIGTABLE_INSTANCE_ID \
     --display-name=$BIGTABLE_DISPLAY_NAME \
     $BIGTABLE_CLUSTER_NUM_NODES_FLAG_VALUE \
     $BIGTABLE_CLUSTER_STORAGE_TYPE_FLAG_VALUE \
-    --instance-type=$BIGTABLE_INSTANCE_TYPE 
+    --instance-type=$BIGTABLE_INSTANCE_TYPE
 
 
 if [ -z $(which cbt) ]; then
@@ -88,5 +88,3 @@ cat > bigquery_table_def.json << EOL
     }
 }
 EOL
-
-
